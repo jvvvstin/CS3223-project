39aae76a9885f27001a13831a3a2e3d871d38b6a
Justin <justin80@live.com.sg>
2026-09-06 16:17:03 +0800

    Switch to HeuristicQueryPlanner and IndexUpdatePlanner pairs

--- src/simpledb/server/SimpleDB.java
+++ src/simpledb/server/SimpleDB.java
@@ -3,8 +3,10 @@ package simpledb.server;
 import java.io.File;
 import simpledb.buffer.BufferMgr;
 import simpledb.file.FileMgr;
+import simpledb.index.planner.IndexUpdatePlanner;
 import simpledb.log.LogMgr;
 import simpledb.metadata.MetadataMgr;
+import simpledb.opt.HeuristicQueryPlanner;
 import simpledb.plan.*;
 import simpledb.tx.Transaction;
 
@@ -53,10 +55,10 @@ public class SimpleDB {
          tx.recover();
       }
       mdm = new MetadataMgr(isnew, tx);
-     QueryPlanner qp = new BasicQueryPlanner(mdm);
-     UpdatePlanner up = new BasicUpdatePlanner(mdm);
-   //  QueryPlanner qp = new HeuristicQueryPlanner(mdm);
-   //  UpdatePlanner up = new IndexUpdatePlanner(mdm);
+//      QueryPlanner qp = new BasicQueryPlanner(mdm);
+//      UpdatePlanner up = new BasicUpdatePlanner(mdm);
+      QueryPlanner qp = new HeuristicQueryPlanner(mdm);
+      UpdatePlanner up = new IndexUpdatePlanner(mdm);
       planner = new Planner(qp, up);
       tx.commit();
    }
